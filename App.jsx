098c556558be87382c83bcdad6a332b43cdbde48
lenaawsx <rodukovatatana84@gmail.com>
2025-11-28 10:32:05 +0200

    App.jsx

--- App.jsx
+++ App.jsx
@@ -1,12 +1,19 @@
 import "./styles.css";
-
-
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AddTaskForm from "./AddTaskForm";
 import ToDoList from "./ToDoList";
 
 function App() {
-  const [t, st] = useState([]);
+  const [t, st] = useState(() => {
+    // Читаем задачи из localStorage при инициализации состояния
+    const saved = localStorage.getItem("tasks");
+    return saved ? JSON.parse(saved) : [];
+  });
+
+  // при каждом изменении t — сохраняем в localStorage
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(t));
+  }, [t]);
 
   const a = (x) => {
     st([...t, { id: Date.now(), text: x }]);
@@ -26,4 +33,3 @@ function App() {
 }
 
 export default App;
-
